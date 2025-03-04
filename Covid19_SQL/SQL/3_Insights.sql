f78b6ff952154f62ebd0e8b1c1c59fff86dddd3f
Mimansa Pandey <65596195+Mimansapandey@users.noreply.github.com>
2025-03-04 01:31:17 +0000

    Update 3_Insights.sql

---
+++ Covid19_SQL/SQL/3_Insights.sql
@@ -0,0 +1,55 @@
+-- Countries with Highest Infection Rates
+SELECT 
+    Location, 
+    MAX(total_cases) AS HighestInfectionCount, 
+    population, 
+    MAX((total_cases / NULLIF(population, 0)) * 100) AS PercentPopulationInfected
+FROM PortfolioProject.CovidDeaths
+GROUP BY Location, population
+ORDER BY PercentPopulationInfected DESC
+
+-- Countries with Highest Death Count per Population
+
+SELECT Location, MAX(total_deaths) AS TotalDeathCount 
+FROM PortfolioProject.CovidDeaths
+WHERE continent is not NULL
+GROUP BY Location
+ORDER BY TotalDeathCount DESC
+
+-- Exploring Data on the basis of Continent
+
+SELECT continent, MAX(total_deaths) AS TotalDeathCount 
+FROM PortfolioProject.CovidDeaths
+WHERE continent is not NULL
+GROUP BY continent
+ORDER BY TotalDeathCount DESC
+
+-- Continents with Highest Death Counts
+
+SELECT continent, MAX(total_deaths) AS TotalDeathCount 
+FROM PortfolioProject.CovidDeaths
+WHERE continent is not NULL
+GROUP BY continent
+ORDER BY TotalDeathCount DESC
+
+
+-- Global Numbers
+SELECT 
+    date, 
+    SUM(new_cases) AS total_cases, 
+    SUM(new_deaths) AS total_deaths, 
+    (SUM(new_deaths) / NULLIF(SUM(new_cases), 0)) * 100 AS DeathPercentage
+FROM PortfolioProject.CovidDeaths
+WHERE continent IS NOT NULL
+GROUP BY date
+ORDER BY 1, 2;
+
+-- GLOBAL TOTAL 
+
+SELECT 
+    SUM(new_cases) AS total_cases, 
+    SUM(new_deaths) AS total_deaths, 
+    (SUM(new_deaths) / NULLIF(SUM(new_cases), 0)) * 100 AS DeathPercentage
+FROM PortfolioProject.CovidDeaths
+WHERE continent IS NOT NULL
+ORDER BY 1, 2;
