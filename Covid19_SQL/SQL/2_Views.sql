cdb6a4b6e1d6129aad8365fd24ca0e303001526e
Mimansa Pandey <65596195+Mimansapandey@users.noreply.github.com>
2025-03-10 18:21:40 +0000

    Update 2_Views.sql

--- Covid19_SQL/SQL/2_Views.sql
+++ Covid19_SQL/SQL/2_Views.sql
@@ -1,5 +1,18 @@
 -- Creating Views for Key COVID-19 Insights
 
+-- View for total cases and deaths by date (Global Level)
+CREATE VIEW Global_CovidTrends AS
+SELECT 
+    date, 
+    SUM(new_cases) AS TotalNewCases, 
+    SUM(new_deaths) AS TotalNewDeaths,
+    SUM(total_cases) AS CumulativeCases,
+    SUM(total_deaths) AS CumulativeDeaths
+FROM PortfolioProject.CovidDeaths
+WHERE continent IS NOT NULL
+GROUP BY date
+ORDER BY date;
+
 -- View: Maximum Death Rate Per Country
 CREATE VIEW MaxDeathRatePerCountry AS 
 SELECT 
